332beea387679cb2c3a4a725dc9544151ff25449
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-10 14:14:01 -0400

    displayed name price and category

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import jsondata from './data.json';
+import { toThrowErrorMatchingSnapshot } from 'jest-snapshot';
 
-function App() {
+class App extends React.Component {
+ 
+  state = {
+    products: jsondata.data,
+  };
+
+showProducts = () => {
+  return this.state.products.map(each => {
+    return(
+      <li>
+        {each.name} - {each.price} - {each.category}
+      </li>
+      
+    );
+  });
+};
+
+search = (e) => {
+  console.log(e.target.value);
+  let filteredProduct = jsondata.data.filter((eachData) => {
+    return eachData.name.toLowerCase().includes(e.target.value.toLowerCase());
+  });
+  this.setState ({products: filteredProduct});
+};
+
+ 
+  render(){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+       name="search" 
+       type="text"
+       placeholder="search"
+       onChange={this.search}/>
+      {this.showProducts()}
     </div>
   );
+
+  }
+  
 }
 
 export default App;
